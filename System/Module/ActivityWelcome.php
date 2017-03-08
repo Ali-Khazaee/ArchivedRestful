b9c57ad1097db0b39b3df3b644876aca7a55674a
Biogram <Biogram@Biogram.co>
2017-03-08 05:37:27 +0330

    Sign Google

--- System/Module/ActivityWelcome.php
+++ System/Module/ActivityWelcome.php
@@ -136,90 +136,56 @@
 
     function ActivityWelcomeSignInGoogle($App)
     {
-        require_once ROOT. 'aaa/vendor/autoload.php';
-    
-    $TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjcxMDY4YTFjYzM0OTQ3MmE5ZTdiZmRkNTQ0MDM5NDlmM2VkMjgzY2UifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJpYXQiOjE0ODg5MDAyMjUsImV4cCI6MTQ4ODkwMzgyNSwiYXVkIjoiNTkwNjI1MDQ1Mzc5LTlwZ2JjNnI4djA3OTRyaWo1OWpqNTBvMWdwNmlqbnZsLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA4NjMzMTkzMzY4MzkwODcwMDU1IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF6cCI6IjU5MDYyNTA0NTM3OS1rNzBwdG5raGo0OWJhZW81bWtwcnBvZDlhczU4c3JtZS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImVtYWlsIjoiZGV2LmtoYXphZWVAZ21haWwuY29tIiwibmFtZSI6IkFsaSBLaGF6YWVlIiwicGljdHVyZSI6Imh0dHBzOi8vbGg0Lmdvb2dsZXVzZXJjb250ZW50LmNvbS8tUHB1U09hSFBOalkvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQUFvbXZWMWt6T0I2RG92aEpXYnVxMDdYVmllV1FZRzJQZy9zOTYtYy9waG90by5qcGciLCJnaXZlbl9uYW1lIjoiQWxpIiwiZmFtaWx5X25hbWUiOiJLaGF6YWVlIiwibG9jYWxlIjoiZW4ifQ.gPuPph7GFZG89jKTDQFdtF4y2XLFl6-XP5kFuc-kvz14k69J42RCZRCCOBo4_-wEBTizZno57nf8pTbAN3tlbg30HfReQpdlJ5DigbDv1Jy8UvlovMFkqMBjnAIU9eCeCMjQa3Du82wGZxDKMqzOLU4kJXljqwgUHWJ0gv3LHubFlMwtmXmrCMB4UWE2_PVhdIqMeQYK0vDVrwcQydc--i9XVO9aoh_KdYZ0P4vUWhXE0JJD2C3blbtqSZDZut24kXXecdyJ07chgHW9n4LklHrd7rjv83QO27MicmFrNRJ0bpebb9EPcdUFpnOnN1xGP3IspUXC2y2YSB-0B8Qjxw";
-
-    $Client = new Google_Client();
-    $PayLoad = $Client->verifyIdToken($TOKEN);
-
-    echo "<pre>";
-    var_dump($PayLoad);
-    echo "</pre>";
-
-    if ($PayLoad)
-    {
-        $userid = $PayLoad['sub'];
-        // If request specified a G Suite domain:
-        //$domain = $payload['hd'];
-    }
-    else
-    {
-        // Invalid ID token
-    }
-    
-        $Username = isset($_POST["Username"]) ? strtolower($_POST["Username"]) : NULL;
-        $Password = isset($_POST["Password"]) ? $_POST["Password"] : NULL;
+        $Token = isset($_POST["Token"]) ? $_POST["Token"] : NULL;
         $Session = isset($_POST["Session"]) ? $_POST["Session"] : NULL;
 
-        if (!isset($Username) || empty($Username))
+        if (!isset($Token) || empty($Token))
             JSON(["Message" => 1]);
 
-        if (!isset($Password) || empty($Password))
+        require_once(ROOT. 'System/Library/GoogleAPI/vendor/autoload.php');
+
+        $Client = new Google_Client();
+        $PayLoad = $Client->verifyIdToken($Token);
+
+        if (!$PayLoad)
             JSON(["Message" => 2]);
 
-        if (strlen($Username) <= 2)
+        if ($PayLoad['iss'] != 'accounts.google.com' && $PayLoad['iss'] != 'https://accounts.google.com'):
             JSON(["Message" => 3]);
 
-        if (strlen($Username) >= 33)
+        if ($PayLoad['aud'] != '590625045379-9pgbc6r8v0794rij59jj50o1gp6ijnvl.apps.googleusercontent.com'):
             JSON(["Message" => 4]);
-
-        if (strlen($Password) <= 4)
-            JSON(["Message" => 5]);
-
-        if (strlen($Password) >= 33)
-            JSON(["Message" => 6]);
-
-        if (!preg_match("/^(?![^A-Za-z])(?!.*\.\.)[A-Za-z0-9_.]+(?<![^A-Za-z])$/", $Username))
-            JSON(["Message" => 7]);
-
-        $App->RateLimit->Call('ActivityWelcomeSignInQuery.1.3000');
-
-        $Account = $App->DB->Find('account', ['Username' => $Username])->toArray();
-
-        if (empty($Account))
-            JSON(["Message" => 8]);
-
-        if (!password_verify($Password, $Account[0]->Password))
-            JSON(["Message" => 9]);
 
         if (!isset($Session) || empty($Session))
             $Session = "Unknown - " . $_SERVER['REMOTE_ADDR'];
         else
             $Session .= " - " . $_SERVER['REMOTE_ADDR'];
 
-        $ID = $Account[0]->_id->__toString();
-        $Token = $App->Auth->CreateToken(["ID" => $ID]);
+        $GoogleID = $PayLoad['sub'];
+        $App->RateLimit->Call('ActivityWelcomeSignInGoogleQuery.1.3000');
+        $Account = $App->DB->Find('account', ['GoogleID' => $GoogleID])->toArray();
 
-        $App->DB->Update('account', ['_id' => new MongoDB\BSON\ObjectID($ID)], ['$push' => ['Session' => ['Name' => $Session, 'Token' => $Token, 'CreatedTime' => time()]]]);
+        if (empty($Account))
+        {
+            $App->RateLimit->Call('ActivityWelcomeSignInGoogleCreated.1.60000');
 
-        JSON(["Message" => 1000, "Token" => $Token, "AccountID" => $ID]);
+            $ID = $App->DB->Insert('account', ['GoogleID' => $GoogleID, 'Email' => $PayLoad['email'], 'CreatedTime' => time()])->__toString();
+
+            $Token = $App->Auth->CreateToken(["ID" => $ID]);
+
+            $App->DB->Update('account', ['_id' => new MongoDB\BSON\ObjectID($ID)], ['$push' => ['Session' => ['Name' => $Session, 'Token' => $Token, 'CreatedTime' => time()]]]);
+
+            JSON(["Message" => 1000, "Token" => $Token, "AccountID" => $ID]);
+        }
+        else
+        {
+            $ID = $Account[0]->_id->__toString();
+
+            $Token = $App->Auth->CreateToken(["ID" => $ID]);
+
+            $App->DB->Update('account', ['_id' => new MongoDB\BSON\ObjectID($ID)], ['$push' => ['Session' => ['Name' => $Session, 'Token' => $Token, 'CreatedTime' => time()]]]);
+
+            JSON(["Message" => 1000, "Token" => $Token, "AccountID" => $ID]);
+        }
     }
-
-    /*function SignOut($App)
-    {
-        $Token = $_SERVER['HTTP_TOKEN'];
-
-        if (!isset($Token) || empty($Token))
-            JSON(["Message" => 1]);
-
-        $Decode = $App->Auth->Decode($Token);
-        $ID = $Decode->ID;
-
-        $App->DB->Update('account', ['_id' => new MongoDB\BSON\ObjectID($ID)], ['$pull' => ['Session' => ["Token" => $Token]]]);
-
-        Logger($App, 'SignOut',['UserID' => $ID]);
-
-        JSON(["Message" => 1000)]);
-    }*/
 ?>
