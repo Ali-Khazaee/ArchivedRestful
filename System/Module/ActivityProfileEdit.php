715299f8da0b35a05b46b24d4b4f3823cf368c44
Biogram <Biogram@Biogram.co>
2017-03-14 02:07:51 +0330

    Update

--- System/Module/ActivityProfileEdit.php
+++ System/Module/ActivityProfileEdit.php
@@ -11,8 +11,8 @@
                         "Description" => isset($User[0]->Description) ? $User[0]->Description : "",
                         "Link"        => isset($User[0]->Link) ? $User[0]->Link : "",
                         "Email"       => isset($User[0]->Email) ? $User[0]->Email : "",
-                        "BackGround"  => isset($User[0]->BackGround) ? $User[0]->BackGround : "",
-                        "Profile"     => isset($User[0]->Profile) ? $User[0]->Profile : "");
+                        "BackGround"  => isset($User[0]->ImageBackGround) ? $User[0]->ImageBackGround : "",
+                        "Profile"     => isset($User[0]->ImageProfile) ? $User[0]->ImageProfile : "");
 
         JSON(["Message" => 1000, "Result" => json_encode($Result)]);
     }
@@ -24,53 +24,115 @@
         $Link = isset($_POST["Link"]) ? strtolower($_POST["Link"]) : NULL;
         $Email = isset($_POST["Email"]) ? $_POST["Email"] : NULL;
 
+        $OldEmail = false;
+        $OldUsername = false;
+
         if (!isset($Username) || empty($Username))
-            JSON(["Message" => 1]);
+            $OldUsername = true;
 
         if (strlen($Username) <= 2)
-            JSON(["Message" => 2]);
+            $OldUsername = true;
 
         if (strlen($Username) >= 33)
-            JSON(["Message" => 3]);
+            $OldUsername = true;
 
         if (!preg_match("/^(?![^A-Za-z])(?!.*\.\.)[A-Za-z0-9_.]+(?<![^A-Za-z])$/", $Username))
-            JSON(["Message" => 4]);
+            $OldUsername = true;
 
         if (!isset($Email) || empty($Email))
-            JSON(["Message" => 5]);
+            $OldEmail = true;
 
         if (!filter_var($Email, FILTER_VALIDATE_EMAIL))
-            JSON(["Message" => 6]);
+            $OldEmail = true;
 
         if (strlen($Email) >= 65)
-            JSON(["Message" => 7]);
+            $OldEmail = true;
 
-        $FileName = $_FILES['ImageProfile']['name'];
-        $FileSize = $_FILES['ImageProfile']['size'];
-        $FileTemp = $_FILES['ImageProfile']['tmp_name'];
-        $FileType = $_FILES['ImageProfile']['type'];
+        $OwnerID = new MongoDB\BSON\ObjectID($App->Auth->ID);
+        $ImageProfile = NULL;
+        $ImageBackGround = NULL;
 
-        if (!in_array(strtolower(pathinfo($FileName, PATHINFO_EXTENSION)), array("jpeg", "jpg")))
-            continue;
+        if (isset($_FILES['ImageProfile']))
+        {
+            $FileName = $_FILES['ImageProfile']['name'];
+            $FileSize = $_FILES['ImageProfile']['size'];
+            $FileTemp = $_FILES['ImageProfile']['tmp_name'];
+            $FileType = $_FILES['ImageProfile']['type'];
 
-        if ($FileType != "image/jpeg")
-            continue;
+            if (!in_array(strtolower(pathinfo($FileName, PATHINFO_EXTENSION)), array("jpeg", "jpg")))
+                break;
 
-        if ($FileSize > 2097152)
-            continue;
+            if ($FileType != "image/jpeg")
+                break;
 
-        $ImageCount++;
-        $Server = Upload::GetBestServer();
+            if ($FileSize > 2097152)
+                break;
 
-        $Channel = curl_init();
-        curl_setopt($Channel, CURLOPT_URL, $Server);
-        curl_setopt($Channel, CURLOPT_HEADER, false);
-        curl_setopt($Channel, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($Channel, CURLOPT_POSTFIELDS, ["ACTION" => "UPLOAD_IMAGE", "TOKEN" => Upload::GetServerToken($Server), "FOLDER" => $App->Auth->ID, "FILE" => new CurlFile($FileTemp, $FileType)]);
-        $URL = curl_exec($Channel);
-        curl_close($Channel);
+            $Server = Upload::GetBestServer();
 
-        array_push($Data, ($Server . $URL));
+            $Channel = curl_init();
+            curl_setopt($Channel, CURLOPT_URL, $Server);
+            curl_setopt($Channel, CURLOPT_HEADER, false);
+            curl_setopt($Channel, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($Channel, CURLOPT_POSTFIELDS, ["ACTION" => "UPLOAD_IMAGE", "TOKEN" => Upload::GetServerToken($Server), "FOLDER" => $OwnerID, "FILE" => new CurlFile($FileTemp, $FileType)]);
+            $URL = curl_exec($Channel);
+            curl_close($Channel);
+
+            $ImageProfile = $Server . $URL;
+        }
+        
+        if (isset($_FILES['ImageBackGround']))
+        {
+            $FileName = $_FILES['ImageBackGround']['name'];
+            $FileSize = $_FILES['ImageBackGround']['size'];
+            $FileTemp = $_FILES['ImageBackGround']['tmp_name'];
+            $FileType = $_FILES['ImageBackGround']['type'];
+
+            if (!in_array(strtolower(pathinfo($FileName, PATHINFO_EXTENSION)), array("jpeg", "jpg")))
+                break;
+
+            if ($FileType != "image/jpeg")
+                break;
+
+            if ($FileSize > 2097152)
+                break;
+
+            $Server = Upload::GetBestServer();
+
+            $Channel = curl_init();
+            curl_setopt($Channel, CURLOPT_URL, $Server);
+            curl_setopt($Channel, CURLOPT_HEADER, false);
+            curl_setopt($Channel, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($Channel, CURLOPT_POSTFIELDS, ["ACTION" => "UPLOAD_IMAGE", "TOKEN" => Upload::GetServerToken($Server), "FOLDER" => $OwnerID, "FILE" => new CurlFile($FileTemp, $FileType)]);
+            $URL = curl_exec($Channel);
+            curl_close($Channel);
+
+            $ImageBackGround = $Server . $URL;
+        }
+
+        $Account = $App->DB->Find('account', ['_id' => $OwnerID])->toArray();
+
+        if ($OldUsername && isset($Account[0]->Username))
+            $Username = $Account[0]->Username;
+
+        if ($OldEmail)
+            $Email = $Account[0]->Email;
+
+        if ($ImageProfile == NULL && isset($Account[0]->ImageProfile))
+            $ImageProfile = $Account[0]->ImageProfile;
+
+        if ($ImageBackGround == NULL && isset($Account[0]->ImageBackGround))
+            $ImageBackGround = $Account[0]->ImageBackGround;
+
+        $App->DB->Update('account', ['_id' => $OwnerID], ['$set' =>
+                                                         ['Username' => $Username,
+                                                          'Email' => $Email,
+                                                          'ImageProfile' => $ImageProfile,
+                                                          'ImageBackGround' => $ImageBackGround,
+                                                          'Description' => $Description,
+                                                          'Link' => $Link]]);
+
+        JSON(["Message" => 1000]);
     }
 
     function ActivityProfileEditDeleteProfile($App)
