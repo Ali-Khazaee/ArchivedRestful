063b2db260ea169445ae8531643a0753b8f27a81
BioGram <BioGram>
2017-08-09 20:27:22 +0430

    Update

--- System/Config/DataBase.php
+++ System/Config/DataBase.php
@@ -1,7 +1,7 @@
 <?php
     if (!defined("ROOT")) { exit(); }
 
-    define("DB_HOST", "5.160.219.220");
+    define("DB_HOST", "5.160.219.219");
     define("DB_PORT", "65001");
     define("DB_NAME", "BioGram");
     define("DB_USERNAME", "BioGram");
